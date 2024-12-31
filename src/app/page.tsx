31298ff2c25190b97231daa171fa28f6d53557ba
San <sanho45600@gmail.com>
2024-12-31 22:12:00 +0800

    feat: temp custom update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,18 +15,32 @@ import {
 } from "@/components/ui/select";
 import { IoMdAdd } from "react-icons/io";
 import { RxCross1 } from "react-icons/rx";
+import { HiMinus } from "react-icons/hi";
 
 interface CardData {
   id: number;
+  name: string;
   first: string;
   second: string;
-  result: string;
+  specialFirst: string;
+  specialSecond: string;
+  totalResult: string;
+  baseResult: string;
+  specialResult: string;
+  isSpecial: boolean;
 }
 
 export interface LogEntry {
   rollName: string;
+  name: string;
   date: string;
-  rolls: { dice: string; result: number }[];
+  rolls: {
+    dice: string;
+    specialDice: string;
+    totalResult: string;
+    baseResult: string;
+    specialResult: string;
+  }[];
 }
 
 export default function Home() {
@@ -35,15 +49,36 @@ export default function Home() {
   const [rollName, setRollName] = useState("");
 
   const [cards, setCards] = useState<CardData[]>([
-    { id: 0, first: "", second: "", result: "" },
+    {
+      id: 0,
+      name: "",
+      first: "",
+      second: "",
+      specialFirst: "",
+      specialSecond: "",
+      totalResult: "",
+      baseResult: "",
+      specialResult: "",
+      isSpecial: false,
+    },
   ]);
 
   const firstArray = Array.from({ length: 24 }, (_, i) => i + 1);
   const secondArray = Array.from({ length: 100 }, (_, i) => i + 1);
 
-  const callback = (rolls: { dice: string; result: number }[]) => {
+  const callback = (
+    rolls: {
+      dice: string;
+      specialDice: string;
+      totalResult: string;
+      baseResult: string;
+      specialResult: string;
+    }[],
+    name: string
+  ) => {
     const newEntry: LogEntry = {
       rollName: rollName,
+      name: name,
       date: getDate(),
       rolls: rolls,
     };
@@ -67,9 +102,12 @@ export default function Home() {
           i === index
             ? {
                 ...c,
-                result: getRandomInt(
+                baseResult: getRandomInt(
                   Number(c.first) * Number(c.second)
                 ).toString(),
+                specialResult: c.isSpecial
+                  ? getRandomInt(Number(c.first) * Number(c.second)).toString()
+                  : "0",
               }
             : c
         )
@@ -77,17 +115,36 @@ export default function Home() {
       await new Promise((resolve) => setTimeout(resolve, 10));
       count++;
     }
-    let total = 0;
+    let base = 0;
+    let special = 0;
+
     for (let i = 0; i < Number(cards[index].first); i++) {
-      total += getRandomInt(Number(cards[index].second));
+      base += getRandomInt(Number(cards[index].second));
     }
     setCards((prev) =>
-      prev.map((c, i) => (i === index ? { ...c, result: total.toString() } : c))
+      prev.map((c, i) =>
+        i === index ? { ...c, baseResult: base.toString() } : c
+      )
     );
+    if (cards[index].isSpecial) {
+      for (let i = 0; i < Number(cards[index].specialFirst); i++) {
+        special += getRandomInt(Number(cards[index].specialSecond));
+      }
+      setCards((prev) =>
+        prev.map((c, i) =>
+          i === index ? { ...c, specialResult: special.toString() } : c
+        )
+      );
+    }
     setRolling(false);
     return {
       dice: `${cards[index].first}d${cards[index].second}`,
-      result: total,
+      specialDice: !cards[index].isSpecial
+        ? ""
+        : `${cards[index].specialFirst}d${cards[index].specialFirst}`,
+      baseResult: base.toString(),
+      specialResult: special.toString(),
+      totalResult: (base + special).toString(),
     };
   };
 
@@ -98,96 +155,200 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen items-center justify-center flex-row  bg-[#242424]">
-      <div className="flex flex-col gap-8 flex-wrap p-12 w-[50%] justify-start items-center ">
-        {cards.map((cardId, index) => (
-          <Card
-            key={index}
-            className="flex relative overflow-visible flex-row gap-4 w-full justify-between items-center border p-12 text-black"
-          >
-            {cards.length > 1 && (
+      <div className="flex flex-col">
+        <div className="grid grid-cols-2 gap-4 p-12 w-full auto-rows-min">
+          {cards.map((cardId, index) => (
+            <Card
+              key={index}
+              className="flex relative overflow-visible flex-row gap-4 w-full justify-between items-center border p-4 text-black"
+            >
               <button
                 onClick={(e) => {
                   e.stopPropagation();
-                  setCards((prev) => prev.filter((_, i) => i !== index));
-                }}
-                className="absolute -right-1 -top-1 bg-red-500 rounded-full flex justify-center items-center w-4 h-4"
-              >
-                <RxCross1 size={10} />
-              </button>
-            )}
-            <div className="flex gap-2 items-center">
-              <Select
-                onValueChange={(val) => {
-                  setCards((prev) =>
-                    prev.map((c, i) => (i === index ? { ...c, first: val } : c))
-                  );
-                }}
-              >
-                <SelectTrigger className="w-[72px] !min-w-0">
-                  <SelectValue placeholder="...">
-                    {cards[index].first}
-                  </SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  {firstArray.map((val) => (
-                    <SelectItem key={val} value={val.toString()}>
-                      {val}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-              <span className="text-white">d</span>
-              <Select
-                onValueChange={(val) => {
                   setCards((prev) =>
                     prev.map((c, i) =>
-                      i === index ? { ...c, second: val } : c
+                      i === index
+                        ? { ...c, isSpecial: !cards[index].isSpecial }
+                        : c
                     )
                   );
                 }}
+                className="absolute right-5 -top-1 bg-white rounded-full flex justify-center items-center w-4 h-4"
               >
-                <SelectTrigger className="w-[72px] !min-w-0">
-                  <SelectValue placeholder="...">
-                    {cards[index].second}
-                  </SelectValue>
-                </SelectTrigger>
-                <SelectContent>
-                  {secondArray.map((val) => (
-                    <SelectItem key={val} value={val.toString()}>
-                      {val}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="text-white">
-              {handleError(Number(cards[index].result))}
-            </div>
-            <Button
-              className=""
-              isDisabled={
-                Number(cards[index].first) <= 0 ||
-                Number(cards[index].second) <= 0 ||
-                rolling
-              }
-              onClick={async () => {
-                const result = await handleRoll(index);
-                if (result) callback([result]);
-              }}
-            >
-              Start
-            </Button>
-          </Card>
-        ))}
+                {!cards[index].isSpecial ? (
+                  <IoMdAdd size={10} />
+                ) : (
+                  <HiMinus size={10} />
+                )}
+              </button>
+              {cards.length > 1 && (
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setCards((prev) => prev.filter((_, i) => i !== index));
+                  }}
+                  className="absolute -right-1 -top-1 bg-red-500 rounded-full flex justify-center items-center w-4 h-4"
+                >
+                  <RxCross1 size={10} />
+                </button>
+              )}
+              <Input
+                className="w-24"
+                value={cards[index].name}
+                onChange={(e) => {
+                  setCards((prev) =>
+                    prev.map((c, i) =>
+                      i === index ? { ...c, name: e.target.value } : c
+                    )
+                  );
+                }}
+              />
+              <div className="flex flex-col gap-2">
+                <div className="flex gap-2 items-center">
+                  <Select
+                    onValueChange={(val) => {
+                      setCards((prev) =>
+                        prev.map((c, i) =>
+                          i === index ? { ...c, first: val } : c
+                        )
+                      );
+                    }}
+                  >
+                    <SelectTrigger className="w-[72px] !min-w-0">
+                      <SelectValue placeholder="...">
+                        {cards[index].first}
+                      </SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      {firstArray.map((val) => (
+                        <SelectItem key={val} value={val.toString()}>
+                          {val}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <span className="text-white">d</span>
+                  <Select
+                    onValueChange={(val) => {
+                      setCards((prev) =>
+                        prev.map((c, i) =>
+                          i === index ? { ...c, second: val } : c
+                        )
+                      );
+                    }}
+                  >
+                    <SelectTrigger className="w-[72px] !min-w-0">
+                      <SelectValue placeholder="...">
+                        {cards[index].second}
+                      </SelectValue>
+                    </SelectTrigger>
+                    <SelectContent>
+                      {secondArray.map((val) => (
+                        <SelectItem key={val} value={val.toString()}>
+                          {val}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <div className="text-white">
+                    {handleError(Number(cards[index].baseResult))}
+                  </div>
+                </div>
+                {cards[index].isSpecial && (
+                  <div className="flex gap-2 items-center">
+                    <Select
+                      onValueChange={(val) => {
+                        setCards((prev) =>
+                          prev.map((c, i) =>
+                            i === index ? { ...c, specialFirst: val } : c
+                          )
+                        );
+                      }}
+                    >
+                      <SelectTrigger className="w-[72px] !min-w-0">
+                        <SelectValue placeholder="...">
+                          {cards[index].specialFirst}
+                        </SelectValue>
+                      </SelectTrigger>
+                      <SelectContent>
+                        {firstArray.map((val) => (
+                          <SelectItem key={val} value={val.toString()}>
+                            {val}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <span className="text-white">d</span>
+                    <Select
+                      onValueChange={(val) => {
+                        setCards((prev) =>
+                          prev.map((c, i) =>
+                            i === index ? { ...c, specialSecond: val } : c
+                          )
+                        );
+                      }}
+                    >
+                      <SelectTrigger className="w-[72px] !min-w-0">
+                        <SelectValue placeholder="...">
+                          {cards[index].specialSecond}
+                        </SelectValue>
+                      </SelectTrigger>
+                      <SelectContent>
+                        {secondArray.map((val) => (
+                          <SelectItem key={val} value={val.toString()}>
+                            {val}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                    <div className="text-white">
+                      {handleError(Number(cards[index].specialResult))}
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              <Button
+                className=""
+                isDisabled={
+                  Number(cards[index].first) <= 0 ||
+                  Number(cards[index].second) <= 0 ||
+                  (cards[index].isSpecial &&
+                    (Number(cards[index].specialFirst) <= 0 ||
+                      Number(cards[index].specialSecond) <= 0)) ||
+                  rolling
+                }
+                onClick={async () => {
+                  const result = await handleRoll(index);
+                  if (result) callback([result], cards[index].name);
+                }}
+              >
+                Start
+              </Button>
+            </Card>
+          ))}
+        </div>
+
         <div className="flex justify-between w-full">
           <Button
             isIconOnly
             className="flex justify-center items-center rounded-full"
-            isDisabled={cards.length >= 4}
+            isDisabled={cards.length >= 10}
             onClick={() => {
               setCards((prev) => [
                 ...prev,
-                { id: prev.length, first: "", second: "", result: "" },
+                {
+                  id: prev.length,
+                  name: "",
+                  first: "",
+                  second: "",
+                  specialFirst: "",
+                  specialSecond: "",
+                  baseResult: "",
+                  specialResult: "",
+                  totalResult: "",
+                  isSpecial: false,
+                },
               ]);
             }}
           >
@@ -204,7 +365,12 @@ export default function Home() {
             <Button
               isDisabled={
                 cards.some(
-                  (card) => Number(card.first) <= 0 || Number(card.second) <= 0
+                  (card) =>
+                    Number(card.first) <= 0 ||
+                    Number(card.second) <= 0 ||
+                    (card.isSpecial &&
+                      (Number(card.specialFirst) <= 0 ||
+                        Number(card.specialSecond) <= 0))
                 ) || rolling
               }
               className="flex justify-center items-center"
@@ -212,9 +378,14 @@ export default function Home() {
                 const results = [];
                 for (let i = 0; i < cards.length; i++) {
                   const result = await handleRoll(i);
-                  if (result) results.push(result);
+                  if (result) {
+                    results.push({
+                      ...result,
+                      name: cards[i].name,
+                    });
+                  }
                 }
-                callback(results);
+                callback(results, rollName);
               }}
             >
               Roll all
@@ -256,12 +427,28 @@ export default function Home() {
                     <div>{val.date}</div>
                   </div>
                   <div className="pl-2">
-                    {val.rolls.map((r, i) => (
-                      <div key={i}>
-                        <span className="text-red-500">{r.result}</span>
-                        {` (${r.dice})`}
-                      </div>
-                    ))}
+                    {val.rolls.map((r, i) => {
+                      const maxResult = Math.max(
+                        ...val.rolls.map((roll) => Number(roll.totalResult))
+                      );
+                      return (
+                        <div key={i} className="flex gap-2">
+                          <span>{val.name}</span>
+                          <span
+                            className={
+                              Number(r.totalResult) === maxResult
+                                ? "text-green-500"
+                                : "text-red-500"
+                            }
+                          >
+                            {Number(r.totalResult)}
+                          </span>
+                          {r.specialResult == "0"
+                            ? ` (${r.dice})`
+                            : `[${r.baseResult} (${r.dice}) + ${r.specialResult} (${r.specialDice})]`}
+                        </div>
+                      );
+                    })}
                   </div>
                   <Divider />
                 </div>
--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -22,7 +22,7 @@ export function Log({
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="bg-black text-white">
-        <div className="flex flex-col-reverse items-start pl-4 py-4 overflow-y-scroll h-[95%]">
+        <div className="flex flex-col-reverse items-start border-l-2  ring-white py-4 overflow-y-scroll ">
           {log.map((val, index) => {
             if (!val.rolls) return;
 
@@ -33,12 +33,28 @@ export function Log({
                   <div>{val.date}</div>
                 </div>
                 <div className="pl-2">
-                  {val.rolls.map((r, i) => (
-                    <div key={i}>
-                      <span className="text-red-500">{r.result}</span>
-                      {` (${r.dice})`}
-                    </div>
-                  ))}
+                  {val.rolls.map((r, i) => {
+                    const maxResult = Math.max(
+                      ...val.rolls.map((roll) => Number(r.totalResult))
+                    );
+                    return (
+                      <div key={i} className="flex gap-2">
+                        <span>{val.name}</span>
+                        <span
+                          className={
+                            Number(r.totalResult) === maxResult
+                              ? "text-green-500"
+                              : "text-red-500"
+                          }
+                        >
+                          {r.totalResult}
+                        </span>
+                        {r.specialResult == "0"
+                          ? ` (${r.dice})`
+                          : `[${r.baseResult} (${r.dice}) + ${r.specialResult} (${r.specialDice})]`}
+                      </div>
+                    );
+                  })}
                 </div>
                 <Divider />
               </div>
